518d437693b46f703baf96a609caaddaab08019e
alexsoares4a <73480812+alexsoares4a@users.noreply.github.com>
2021-08-01 20:07:11 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMVC/Controllers/DepartamentosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> listaDepartamentos = new List<Departamento>();
+            listaDepartamentos.Add(new Departamento(1, "Eletronicos"));
+            listaDepartamentos.Add(new Departamento(2, "Informática"));
+
+            return View(listaDepartamentos);
+        }
+    }
+}
---
+++ SalesWebMVC/Models/Departamento.cs
@@ -0,0 +1,14 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Departamento
+    {
+        public int CodigoDepartamento { get; set; }
+        public string NomeDepartamento { get; set; }
+
+        public Departamento(int codigoDepartamento, string nomeDepartamento)
+        {
+            CodigoDepartamento = codigoDepartamento;
+            NomeDepartamento = nomeDepartamento;
+        }
+    }
+}
