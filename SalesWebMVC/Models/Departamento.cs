ccf85e52839d51e213f6c430dfb41beb5d45e2c7
alexsoares4a <73480812+alexsoares4a@users.noreply.github.com>
2021-08-02 22:06:50 -0300

    CRUD Scaffolding

--- SalesWebMVC/Models/Departamento.cs
+++ SalesWebMVC/Models/Departamento.cs
@@ -2,13 +2,7 @@
 {
     public class Departamento
     {
-        public int CodigoDepartamento { get; set; }
+        public int Id { get; set; }
         public string NomeDepartamento { get; set; }
-
-        public Departamento(int codigoDepartamento, string nomeDepartamento)
-        {
-            CodigoDepartamento = codigoDepartamento;
-            NomeDepartamento = nomeDepartamento;
-        }
     }
 }
